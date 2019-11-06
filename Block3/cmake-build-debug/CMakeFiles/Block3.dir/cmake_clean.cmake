f2c8b6cb0713e5479344f4131d861dbbdf0cede8
FlorianJKlein <57286407+FlorianJKlein@users.noreply.github.com>
2019-11-06 15:50:45 +0100

    Add files via upload

---
+++ Block3/cmake-build-debug/CMakeFiles/Block3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Block3.dir/library.c.o"
+  "libBlock3.a"
+  "libBlock3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Block3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Block3/library.h
@@ -0,0 +1,6 @@
+#ifndef BLOCK3_LIBRARY_H
+#define BLOCK3_LIBRARY_H
+
+void hello(void);
+
+#endif //BLOCK3_LIBRARY_H
