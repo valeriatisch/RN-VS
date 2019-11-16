c713d6d28071b9d8f9135af8e655979606b75252
Moritz209 <moritz.209.209@gmail.com>
2019-11-16 21:45:03 +0100

    clinet_moritz
    header wird nichtmehr concatet, sondern einzeln gesendet, kriegt immer nen sigpipe, vlt wegen dem server

--- Block3/client.c
+++ Block3/client.c
@@ -9,8 +9,20 @@
 #include <sys/socket.h>
 
 #include <arpa/inet.h>
+void    print_bits(unsigned char octet)
+{
+    int z = 128, oct = octet;
 
-#define PORT "3490" // the port client will be connecting to
+    while (z > 0)
+    {
+        if (oct & z)
+            write(1, "1", 1);
+        else
+            write(1, "0", 1);
+        z >>= 1;
+    }
+    printf("\n");
+}
 
 #define MAXDATASIZE 100 // max number of bytes we can get at once
 
@@ -32,42 +44,78 @@ int main(int argc, char *argv[]){
     char* send_buffer[100];
 
     if (argc != 5) {
+        printf("%d\n",argc);
         fprintf(stderr,"usage: client hostname key methode value\n");
         exit(1);
     }
-    
-    char* res_key = argv[2];
+    //TODO: key richtig auslesen
+    char* port = argv[2];
     char* call_type = argv[3];
     void* res_value = argv[4];
+    char* res_key = argv[4];
 
-    //alloc  
-    char* header = calloc(1,sizeof(char)*7);
-    
-    if(res_value == 'GET') {
-        header[0] |= 1UL << 2; 
-    } else if(res_value == 'SET') {
-        header[0] |= 1UL << 1;
-    } else if(res_value == 'DEL') {
-        header[0] |= 1UL;
+    printf("key name: %s\n", res_key);
+    //alloc header
+    char* call_header = calloc(1,sizeof(char)*1);
+
+    //set flags in header
+    if(strcmp(call_type,"GET") == 0) {
+        call_header[0] |= 1UL << 2;
+    } else if(strcmp(call_type,"SET") == 0) {
+        call_header[0] |= 1UL << 1;
+    } else if(strcmp(call_type,"DELETE") == 0) {
+        call_header[0] |= 1UL;
     } else
     {
         perror("invalid command\n");
     }
 
-    //merge header and key
-    char* send_data = malloc(sizeof(char)*(strlen(header)+strlen(res_key)));
-    if(send_data == NULL){
-        perror("alloc:send_data");
-        exit(1);
+    print_bits(call_header[0]);
+    //TODO: keylen + valuelen berechen -> in header speichern
+
+    char* key_length_header = calloc(1, sizeof(char)*2);
+    char* value_length_header = calloc(1, sizeof(char)*4);
+
+    short key_len = (short) strlen(res_key);
+
+    printf("key length is: %hu\n",key_len );
+
+    key_length_header[0] = (key_len << 8) & 0xFF;
+    key_length_header[1] = key_len & 0xFF;
+
+    printf("key length:\n");
+    print_bits(key_length_header[0]);
+    print_bits(key_length_header[1]);
+    printf("\n");
+
+
+    // Calculate value size by reading it bytewise till EOF,
+    int value_len = 0;
+    while(getchar() != EOF) {
+        value_len++;
     }
-    snprintf(send_data,sizeof(send_data),"%s%s",header,res_key);
+    printf("Value length is: %d\n", value_len);
+    value_length_header[0] = (value_len >> 24) & 0xFF;
+    value_length_header[1] = (value_len >> 16) & 0xFF;
+    value_length_header[2] = (value_len >> 8) & 0xFF;
+    value_length_header[3] = value_len & 0xFF;
+
+    printf("Value length:\n");
+
+    print_bits(value_length_header[0]);
+    print_bits(value_length_header[1]);
+    print_bits(value_length_header[2]);
+    print_bits(value_length_header[3]);
+
+    printf("\n");
+
 
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
 
-    if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
+    if ((rv = getaddrinfo(argv[1], port, &hints, &servinfo)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return 1;
     }
@@ -84,7 +132,7 @@ int main(int argc, char *argv[]){
             perror("client: connect");
             continue;
         }
-
+        ;
         break;
     }
 
@@ -96,9 +144,29 @@ int main(int argc, char *argv[]){
     //send data to server
     //send header + key
     int tmp = 0;
-    while(tmp < sizeof(send_data)){
-        if(tmp += send(sockfd,send_data,sizeof(send_data)-tmp,0) == -1){
-            perror("send:header+key");
+    // send first byte
+    printf("Debug: sending first byte\n");
+    printf("size of first header: %lu \n", sizeof(call_header)/sizeof(char));
+    while(tmp < sizeof(call_header)){
+        if(tmp += send(sockfd,call_header,1-tmp,0) == -1){
+            perror("sending first byte");
+        }
+    }
+    printf("debug:sending first byte\n");
+
+    tmp = 0;
+    // sending key length
+    while(tmp < sizeof(key_length_header)) {
+        if(tmp += send(sockfd, key_length_header, 2-tmp,0)) {
+            perror("sending value value");
+        }
+    }
+
+    tmp = 0;
+    //sending value length
+    while(tmp < sizeof(value_length_header)) {
+        if(tmp += send(sockfd, value_length_header,4-tmp,1)) {
+            perror("sending value length");
         }
     }
 
@@ -112,6 +180,8 @@ int main(int argc, char *argv[]){
 
     freeaddrinfo(servinfo); // all done with this structure
 
+    printf("---------------------- ALL DATA SENT; READY TO RECEIVE DATE FROM SERVER --------------------\n");
+
     //receive data from server
     if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
         perror("recv");
@@ -120,8 +190,11 @@ int main(int argc, char *argv[]){
 
     buf[numbytes] = '\0';
 
-    printf("client: received '%s'\n",buf);
-
+    //return value for 'GET'
+    //TODO: nur value ohne header printen
+    if(strcmp(call_type,"GET") == 0) {
+        printf("client: received '%s'\n", buf);
+    }
     close(sockfd);
 
     return 0;
