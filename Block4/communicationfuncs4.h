4817a5eebccf3b6288df2a777f02ee2db4d17f85
valeriatisch <57265123+valeriatisch@users.noreply.github.com>
2019-11-25 17:36:07 +0100

    Add files via upload

--- Block4/communicationfuncs4.h
+++ Block4/communicationfuncs4.h
@@ -13,7 +13,7 @@
 char* recv_n_char(int new_fd, int size);
 void send_n_char(int new_fd, char* arr, int size);
 void send_message2client(char* header, int  new_fd, int headerlength);
-int check_datarange(uint16_t hash_key, );
+int check_datarange(uint16_t hash_key, uint16_t node_ID);
 
 
 char* recv_n_char(int new_fd, int size){
@@ -76,47 +76,51 @@ void send_message2client(char* header, int  new_fd, int headerlength){
     char* value = NULL;
     if(valuelen > 0) value = recv_n_char(new_fd, valuelen);
 
-    if(header[0] >> 4) & 0b1 == 1){ // ack bit set --> answer to be sent to client
+    if(requested_del > 0){
+        delete(key, keylen);
+        header[0] |= 0b1000;
+        //send header
         send_n_char(new_fd, header, headerlength);
-        if(requested_get > 0) { // if get request, return key and value aswell
-        send_n_char(new_fd, key, keylen);
-        send_n_char(new_fd, value, valuelen);
-        }
     }
-    else{
-        if(requested_del > 0){
-            delete(key, keylen);
-            header[0] |= 0b1000;
-            //send header
+    else if(requested_set > 0){
+        set(key, keylen, value, valuelen);
+        header[0] |= 0b1000;
+        //send header
+        send_n_char(new_fd, header, headerlength);
+    }
+    else if(requested_get > 0){
+        struct HASH_elem *result = get(key, keylen);
+        header[0] |= 0b1000;
+        //no element found
+        if(result == NULL){
+            //send header without ack
             send_n_char(new_fd, header, headerlength);
         }
-        else if(requested_set > 0){
-            set(key, keylen, value, valuelen);
-            header[0] |= 0b1000;
-            //send header
+            //element found
+        else{
+            uint32_t len = result->value_length;
+            header[3] = (len >> 24) & 0xFF;
+            header[4] = (len >> 16) & 0xFF;
+            header[5] = (len >> 8) & 0xFF;
+            header[6] = len & 0xFF;
             send_n_char(new_fd, header, headerlength);
-        }
-        else if(requested_get > 0){
-            struct HASH_elem *result = get(key, keylen);
-            header[0] |= 0b1000;
-            //no element found
-            if(result == NULL){
-                //send header without ack
-                send_n_char(new_fd, header, headerlength);
-            }
-                //element found
-            else{
-                uint32_t len = result->value_length;
-                header[3] = (len >> 24) & 0xFF;
-                header[4] = (len >> 16) & 0xFF;
-                header[5] = (len >> 8) & 0xFF;
-                header[6] = len & 0xFF;
-                send_n_char(new_fd, header, headerlength);
-                send_n_char(new_fd, result->key, keylen);
-                send_n_char(new_fd, result->value, result->value_length);
-            }
+            send_n_char(new_fd, result->key, keylen);
+            send_n_char(new_fd, result->value, result->value_length);
         }
     }
+}
+
+int check_datarange(uint16_t hash_key, uint16_t self_ID, uint16_t successor_ID, uint16_t predecessor_ID){
+
+    if(predecessor_ID > self_ID){ //I'm the first peer, so pre is larger than me
+        if(hash_key > predecessor_ID) return 1; //hash_key is located between my pre and me -> i'm responsible
+        if(hash_key <= successor_ID && hash_key > self_ID) return 2; //hash_key is located between my succ and me -> my succ is responsible
+    }
+    else if(predecessor_ID < self_ID){ //I'm larger than my pre -> all cases except one
+        if(hash_key > predecessor_ID && hash_key <= self_ID) return 1; //hash_key is located between my pre and me -> i'm responsible
+        if(hash_key <= successor_ID && hash_key > self_ID) return 2; //hash_key is located between my succ and me -> my succ is responsible
+    } else return 3; //I can't see anyone who's responsible -> send lookup to the next peer
+
 }
 
 #endif //BLOCK4_COMMUNICATIONFUNCS4_H
